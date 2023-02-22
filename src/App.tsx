4070a24ccd1a31013224f68fee1a565308bbc16a
Emerson Costa <emerson.esn@gmail.com>
2023-02-22 13:31:32 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
 import styled from "styled-components";
 
 const AppWrapper = styled.div`
@@ -50,25 +51,58 @@ const Bar = styled.span`
   height: 10%;
 `;
 
-const winningCase = [
-  // horizontal
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-
-  // vertical
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-
-  // diagonal
-  [0, 4, 8],
-  [2, 4, 6],
-
-  // reverse diagonal
-  [8, 4, 0],
-  [6, 4, 2],
+const winningCases = [
+  {
+    case: [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ],
+    line: "horizontal",
+  },
+  {
+    case: [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ],
+    line: "vertical",
+  },
+  {
+    case: [
+      [0, 4, 8],
+      [2, 4, 6],
+    ],
+    line: "diagonal",
+  },
+  {
+    case: [
+      [8, 4, 0],
+      [6, 4, 2],
+    ],
+    line: "reverse-diagonal",
+  },
 ];
+
+// [
+//   // horizontal
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+
+//   // vertical
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+
+//   // diagonal
+//   [0, 4, 8],
+//   [2, 4, 6],
+
+//   // reverse diagonal
+//   [8, 4, 0],
+//   [6, 4, 2],
+// ];
 
 function App() {
   const [items, setItems] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
@@ -84,17 +118,22 @@ function App() {
   };
 
   const checkWinner = () => {
-    winningCase.forEach((item) => {
-      if (
-        items[item[0]] === items[item[1]] &&
-        items[item[1]] === items[item[2]] &&
-        items[item[0]] !== 0
-      ) {
-        console.log("winner", turn);
-        setHaveWinner(true);
-        alert(` ${turn === 1 ? "X" : "O"} is the winner!`);
-      }
+    const haveWinner = winningCases.filter((item) => {
+      return item.case.some((item) => {
+        return (
+          items[item[0]] === items[item[1]] &&
+          items[item[1]] === items[item[2]] &&
+          items[item[0]] !== 0
+        );
+      });
     });
+
+    console.log(haveWinner);
+    if (haveWinner.length === 0) return;
+
+    console.log("winner", turn);
+    setHaveWinner(true);
+    alert(` ${turn === 1 ? "X" : "O"} is the winner!`);
   };
 
   const checkDraw = () => {
@@ -110,19 +149,57 @@ function App() {
   }, [items]);
 
   return (
-    <AppWrapper>
-      <MainComponent>
-        <Bar />
-        {items.map((item, index) => (
-          <Item
-            onClick={() => !haveWinner && handleClickItem(index)}
-            key={index}
-          >
-            {item === 1 ? "X" : item === 2 ? "O" : ""}
-          </Item>
-        ))}
-      </MainComponent>
-    </AppWrapper>
+    <>
+      <ReactCanvasConfetti
+        style={{
+          position: "fixed",
+          pointerEvents: "none",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+        }}
+        colors={[
+          "#26ccff",
+          "#a25afd",
+          "#ff5e7e",
+          "#88ff5a",
+          "#fcff42",
+          "#ffa62d",
+          "#ff36ff",
+        ]}
+        decay={0.8}
+        drift={0}
+        gravity={1}
+        origin={{
+          x: 0.5,
+          y: 0.5,
+        }}
+        particleCount={100}
+        resize
+        scalar={1}
+        shapes={["circle", "square"]}
+        spread={360}
+        startVelocity={45}
+        ticks={600}
+        angle={1}
+        zIndex={-1}
+        fire={haveWinner}
+      />
+      <AppWrapper>
+        <MainComponent>
+          <Bar />
+          {items.map((item, index) => (
+            <Item
+              onClick={() => !haveWinner && handleClickItem(index)}
+              key={index}
+            >
+              {item === 1 ? "X" : item === 2 ? "O" : ""}
+            </Item>
+          ))}
+        </MainComponent>
+      </AppWrapper>
+    </>
   );
 }
 
